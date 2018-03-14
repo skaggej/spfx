326a5bfd5cca9a44b31093e11d25c3f6c58bfa5f
Eric Skaggs <eskaggs@outlook.com>
2018-03-14 06:18:15 -0700

    working

--- cswp-apolloviewer/src/webparts/nasaApolloMissionViewer/NasaApolloMissionViewerWebPart.ts
+++ cswp-apolloviewer/src/webparts/nasaApolloMissionViewer/NasaApolloMissionViewerWebPart.ts
@@ -11,9 +11,30 @@ import * as strings from 'NasaApolloMissionViewerWebPartStrings';
 
 export interface INasaApolloMissionViewerWebPartProps {
   description: string;
+  selectedMission: string;
 }
 
+import { IMission } from '../../models';
+import { MissionService } from '../../services';
+import { IPropertyPaneTextFieldProps } from '@microsoft/sp-webpart-base';
+
+
 export default class NasaApolloMissionViewerWebPart extends BaseClientSideWebPart<INasaApolloMissionViewerWebPartProps> {
+
+  private selectedMission: IMission;
+
+  private missionDetailElement: HTMLElement;
+
+  protected onInit(): Promise<void>{
+    return new Promise<void>(
+      (
+        resolve: () => void,
+        reject: (error: any) => void
+      ): void => {
+        this.selectedMission = this._getSelectedMission();
+        resolve();
+    });
+  }
 
   public render(): void {
     this.domElement.innerHTML = `
@@ -21,16 +42,21 @@ export default class NasaApolloMissionViewerWebPart extends BaseClientSideWebPar
         <div class="${ styles.container }">
           <div class="${ styles.row }">
             <div class="${ styles.column }">
-              <span class="${ styles.title }">Welcome to SharePoint!</span>
-              <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
+              <span class="${ styles.title }">Apollo Mission Viewer</span>
               <p class="${ styles.description }">${escape(this.properties.description)}</p>
-              <a href="https://aka.ms/spfx" class="${ styles.button }">
-                <span class="${ styles.label }">Learn more</span>
-              </a>
+              <div id="apolloMissionDetails"></div>
             </div>
           </div>
         </div>
       </div>`;
+
+      this.missionDetailElement = document.getElementById('apolloMissionDetails');
+
+      if(this.selectedMission){
+        this._renderMissionDetails(this.missionDetailElement, this.selectedMission);
+      } else {
+        this.missionDetailElement.innerHTML = '';
+      }
   }
 
   protected get dataVersion(): Version {
@@ -50,6 +76,9 @@ export default class NasaApolloMissionViewerWebPart extends BaseClientSideWebPar
               groupFields: [
                 PropertyPaneTextField('description', {
                   label: strings.DescriptionFieldLabel
+                }),
+                PropertyPaneTextField('selectedMission', <IPropertyPaneTextFieldProps> {
+                  label: "Apollo Mission to Show"
                 })
               ]
             }
@@ -57,5 +86,35 @@ export default class NasaApolloMissionViewerWebPart extends BaseClientSideWebPar
         }
       ]
     };
+  }
+
+  private _getSelectedMission(): IMission {
+    const selectedMissionId: string = (this.properties.selectedMission)
+    ? this.properties.selectedMission
+    : 'AS-506';
+    return MissionService.getMission(selectedMissionId);
+  }
+
+  private _renderMissionDetails(element: HTMLElement, mission: IMission): void {
+    element.innerHTML = `
+      <p class="ms-font-m">
+        <span class="ms-fontWeight-semibold">Mission: </span>
+        ${escape(mission.name)}
+      </p>
+      <p class="ms-font-m">
+        <span class="ms-fontWeight-semibold">Duration: </span>
+        ${escape(this._getMissionTimeline(mission))}
+      </p>
+      <a href="${mission.wiki_href}" target="_blank" class="${styles.button}">
+        <span class="${styles.label}">Learn more about ${escape(mission.name)} on Wikipedia >></span>
+      </a>
+    `;
+  }
+
+  private _getMissionTimeline(mission: IMission): string {
+    let missionDate = mission.end_date !== ''
+    ? `${mission.launch_date.toString()} - ${mission.end_date.toString()}`
+    : `${mission.launch_date.toString()}`;
+    return missionDate;
   }
 }
--- cswp-apolloviewer/src/webparts/nasaApolloMissionViewer/loc/en-us.js
+++ cswp-apolloviewer/src/webparts/nasaApolloMissionViewer/loc/en-us.js
@@ -1,7 +1,7 @@
 define([], function() {
   return {
-    "PropertyPaneDescription": "Description",
-    "BasicGroupName": "Group Name",
+    "PropertyPaneDescription": "Displays information about a selected NASA Apollo mission",
+    "BasicGroupName": "Mission Configuration",
     "DescriptionFieldLabel": "Description Field"
   }
 });
